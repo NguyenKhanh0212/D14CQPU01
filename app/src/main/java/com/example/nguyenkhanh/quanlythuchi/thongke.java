c926c5398742847a3b845e559d4d2c92cf944ccb
NguyenKhanh <N14DCPT164@student.ptithcm.edu.vn>
2018-04-06 23:59:08 +0700

    Quản lý thu chi
    MSSV : N14DCPT164
    Nguyễn Thị Khánh Trang

---
+++ app/src/main/java/com/example/nguyenkhanh/quanlythuchi/thongke.java
@@ -0,0 +1,38 @@
+package com.example.nguyenkhanh.quanlythuchi;
+
+
+public class thongke {
+    String loaithongke ;
+    Integer tien ;
+
+
+    public thongke(String loaithongke, Integer tien) {
+        this.loaithongke = loaithongke;
+        this.tien = tien;
+    }
+
+    public thongke() {
+
+    }
+
+    public String getLoaithongke() {
+        return loaithongke;
+    }
+
+    public void setLoaithongke(String loaithongke) {
+        this.loaithongke = loaithongke;
+    }
+
+    public Integer getTien() {
+        return tien;
+    }
+
+    public void setTien(Integer tien) {
+        this.tien = tien;
+    }
+
+    @Override
+    public String toString() {
+        return loaithongke +"-"+ tien ;
+    }
+}
